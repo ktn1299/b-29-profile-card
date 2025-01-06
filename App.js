b5f1765d6a1e8293aa398a56cb522b35bc56c6d1
ktn1299 <ktneditz1299@gmail.com>
2025-01-06 23:22:03 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Profile from './profile';
+
+function App() {
+    return (
+        <div className="App">
+            <Profile />
+        </div>
+    );
+}
+
+export default App;
